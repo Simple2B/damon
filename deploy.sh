b300b259306f6c9825f6dd4da47904f85bcee18e
Nikolay <nchernov@gmail.com>
2020-11-08 22:29:47 +0200

    scp deploy

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+scp -r app damon:site/
+scp config.py damon:site/
+scp .env damon:site/
+scp .flaskenv damon:site/
+scp config.py damon:site/
+scp requirements.txt damon:site/
+scp wsgi.py damon:site/
+
