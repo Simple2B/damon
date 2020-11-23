f64008e73c29c0eab231b555b0d9296c6193497b
Vitaly <dagger180@gmail.com>
2020-11-23 07:31:21 +0000

    remove comments in js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -41,23 +41,4 @@ $(document).ready(function() {
       modal.find('.form').attr('action', target_link);
     });
 
-
-    // $('#modalAssign').on('show.bs.modal', function (event) {
-    //   const button = $(event.relatedTarget); // Button that triggered the modal
-    //   const target_link = button.data('target-link');
-    //   const modal = $(this);
-    //   console.log(button.data('dispatchcustomername'));
-    //   const dispatchCustomerName = document.createTextNode(button.data('dispatch-customer-name'));
-    //   const dispatchJobName = document.createTextNode(button.data('dispatch-job-name'));
-    //   const dispatchJobNumber = document.createTextNode(button.data('dispatch-job-number'));
-    //   document.getElementById('dispatchCustomerNameID').textContent = ""
-    //   document.getElementById('dispatchJobNameID').textContent = ""
-    //   document.getElementById('dispatchJobNumberID').textContent = ""
-
-    //   document.getElementById('dispatchCustomerNameID').appendChild(dispatchCustomerName);
-    //   document.getElementById('dispatchJobNameID').appendChild(dispatchJobName);
-    //   document.getElementById('dispatchJobNumberID').appendChild(dispatchJobNumber);
-    //   modal.find('.form').attr('action', target_link);
-    // });
-
 } );
