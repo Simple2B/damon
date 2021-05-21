9c3831f0d6c0f15905f56d61105f9fe91e3cef85
Nikolay <nchernov@gmail.com>
2021-05-21 12:42:43 +0000

    csrf disabled

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ class ProductionConfig(BaseConfig):
 
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL', 'sqlite:///' + os.path.join(BASE_DIR, 'database.sqlite3'))
-    WTF_CSRF_ENABLED = True
+    WTF_CSRF_ENABLED = False
 
 
 config = dict(
